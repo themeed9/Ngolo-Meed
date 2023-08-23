b6e460d24e11c9c86c01b33d3de4f7909de464bc
unknown <abdulsalamabdulhameed77@gmail.com>
2023-08-23 20:45:56 +0100

    commiting

--- Ngolo.js
+++ Ngolo.js
@@ -1,3 +1,11 @@
-// let H3 = document.createElement('h3');
-// H3.innerHTML = "Welcom to"
-// document.body.appendChild(H3)
+  
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.navlink button');
+
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        buttons.forEach(btn => btn.classList.remove('btnActive'));
+        this.classList.add('btnActive');
+      });
+    });
+  });
